14d8ccde0ac02fb6d2a53208b669a22fee2fe2b2
Dell <ali.hassan.salman.2000@gmail.com>
2025-08-06 18:08:55 +0300

    task3

--- app.js
+++ app.js
@@ -102,15 +102,12 @@ let veryOldInventors = [];
 
 // Complete the exercise in the space below:
 
-// Check your work:
-console.log('Exercise 1 my result: ', veryOldInventors);
-console.log('Exercise 1 correct result: ', );
-
-const oldInventors  = inventors.filter((name) => {
+veryOldInventors  = inventors.filter((name) => {
   return name.year > 1500 && name.year < 1600;
 });
 
-console.log(oldInventors);
+// Check your work:
+console.log('Exercise 1 result: ', veryOldInventors);
 console.log('==========================================');
 
 /*
@@ -132,24 +129,44 @@ let inventorNames = [];
 
 // Complete the exercise in the space below:
 
+inventorNames = inventors.map((element) => {
+ return {
+    first: element.first,
+    last: element.last
+  };
+});
+
 // Check your work:
-console.log('Exercise 2 my result: ', inventorNames);
-console.log('Exercise 2 correct result: ', [
-  { first: 'Albert', last: 'Einstein' },
-  { first: 'Isaac', last: 'Newton' },
-  { first: 'Galileo', last: 'Galilei' },
-  { first: 'Marie', last: 'Curie' },
-  { first: 'Johannes', last: 'Kepler' },
-  { first: 'Nicolaus', last: 'Copernicus' },
-  { first: 'Max', last: 'Planck' },
-  { first: 'Katherine', last: 'Blodgett' },
-  { first: 'Ada', last: 'Lovelace' },
-  { first: 'Sarah E.', last: 'Goode' },
-  { first: 'Lise', last: 'Meitner' },
-  { first: 'Hanna', last: 'Hammarström' },
+console.log('Exercise 2 result: ', inventorNames);
+console.log('==========================================');
+
+/*
+Exercise 3: Array.prototype.sort()
+
+Sort the inventors by birth date in ascending order (from those born furthest in
+the past to those born most recently).
+*/
+
+let sortedByBirthYear = [];
+
+// Complete the exercise in the space below:
+
+// Check your work:
+console.log('Exercise 3 my result: ', sortedByBirthYear);
+console.log('Exercise 3 correct result: ', [
+  { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+  { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+  { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+  { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+  { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+  { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 },
+  { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+  { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+  { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+  { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+  { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+  { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
 ]);
-
-
 
 console.log('==========================================');
 console.log('==========================================');
